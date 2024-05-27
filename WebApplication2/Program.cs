71bd8aedfd83e0ca6b88b86620539ca55b75f824
BinHu <Bhu0002@conestogac.on.ca>
2024-05-27 16:38:39 -0400

    Add code for exceptions handling

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,18 +1,17 @@
 using System;
+using System.Runtime.CompilerServices;
 
 namespace Bin_Test
 {
     public class Pet_Attributes
     {
-        int Hunger;
-        int Happiness;
-        int Health;
 
         public static void Main(string[] args)
         {
-            SelectPet();
-            NameYourPet();
-            MainManu();
+            int PetNum = SelectPet();
+            string PetName = NameYourPet(PetNum);
+            MainManu(PetName);
+                
         }
 
         public static int SelectPet()
@@ -24,30 +23,34 @@ namespace Bin_Test
             Console.WriteLine("");
             Console.Write("Please select: ");
             int Pet = int.Parse(Console.ReadLine());
-            if (Pet != 1 && Pet != 2 && Pet != 3)
+            if (Pet < 1 && Pet > 3)
                 ExceptionHandlingForPet();
             return Pet;
         }
 
-        public static string NameYourPet()
+        public static string NameYourPet(int PetNum)
         {
             String[] PetSelection = {"Monkey", "Elephant", "Tiger"};
-            Console.WriteLine("You've chosen a {PetSelection[SelectPet()]}. What would you like to name your pet?");
+/*            Console.WriteLine(PetSelection[(PetNum) - 1]);*/
+            Console.WriteLine("You've chosen a(n) " + PetSelection[(PetNum)-1] + ". What would you like to name your pet?");
             Console.Write("Input a name: ");
             String PetName = Console.ReadLine();
-            Console.WriteLine("Welcome, " + PetName + "!" + "Let's take good care of him.");
-            return PetName;
+            Console.WriteLine("Welcome, " + PetName + "! " + "Let's take good care of him.");
+            return PetName; 
                 
          }
 
-        public static void MainManu() {
+        public static void MainManu(string PetName) {
             Console.WriteLine("Main Manu:");
-            Console.WriteLine("1. Feed " + NameYourPet());
-            Console.WriteLine("2. Play with " + NameYourPet());
-            Console.WriteLine("3. Let " + NameYourPet() + " Rest");
-            Console.WriteLine("4. Check " + NameYourPet() + "'s Status");
+            Console.WriteLine("1. Feed " + PetName);
+            Console.WriteLine("2. Play with " + PetName);
+            Console.WriteLine("3. Let " + PetName + " Rest");
+            Console.WriteLine("4. Check " + PetName + "'s Status");
             Console.WriteLine("5. Exit");
             Console.Write("Please select: ");
+            int ManuInput = int.Parse(Console.ReadLine());
+            if (ManuInput < 1 && ManuInput > 5)
+                ExceptionHandlingForMainManu(PetName);
 
         }
 
@@ -55,6 +58,12 @@ namespace Bin_Test
         {
             Console.WriteLine("The number you input is wrong!!!");
             SelectPet();
+        }
+
+        public static void ExceptionHandlingForMainManu(string PetName)
+        {
+            Console.WriteLine("The number you input is wrong!!!");
+            MainManu(PetName);
         }
     }
 }
